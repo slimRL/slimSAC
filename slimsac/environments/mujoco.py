4d1635e5ca044c6dcd4fc885e2aa1c1adead5ad4
yogesh1q2w <yogesh120699@gmail.com>
2025-11-11 15:37:39 +0100

    env clean

--- slimsac/environments/mujoco.py
+++ slimsac/environments/mujoco.py
@@ -3,15 +3,15 @@ import numpy as np
 
 
 class Mujoco:
-    def __init__(self, task_name, seed, render_mode=None):
+    def __init__(self, task_name, seed):
         self.rng = np.random.default_rng(seed)
-        self.env = gym.make(task_name + "-v5", render_mode=render_mode)
+        self.env = gym.make(task_name + "-v5")
+
         self.observation_dim = self.env.observation_space.shape[0]
         self.action_dim = self.env.action_space.shape[0]
         self.env.action_space.seed(seed)
         self.action_scaler = float(self.env.env.action_space.high_repr)
 
-    # Called when stored in the replay buffer
     @property
     def observation(self) -> np.ndarray:
         return np.copy(self.state)
